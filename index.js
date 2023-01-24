636d1fa1c6f3c7a64097b90113880853a6b836e5
teklumezgebo <mezge002@umn.edu>
2023-01-24 16:43:50 -0600

    commit tree b9b3ad103a1de4983b077c51d932e9ade3743d41

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+for (let age = 30; age < 40; age++) {
+    console.log("I\'m ${age} years old. Happy birthday to me!")
+}
+
+const gifts = ['teddy bear', 'drone', 'doll']
+
+function wrapGifts(gifts) {
+    for (let i=0; i < gifts.length; i++) {
+        console.log('Wraped ${gifts[i]} and added a bowl')
+    }
+    
+    return gifts
+}
+
+wrapGifts(gifts)
+
+
+function writeCards(names, event) {
+    let greeting = []
+    for (let i = 0; i < names.length; i++) {
+        greeting.push('Thank you, ' + names[i] + ', for the wonderful ' + event +  ' gift!')
+    }
+
+    return greeting
+
+}
+
+function countDown(positiveInteger) {
+    let i = positiveInteger
+    while (i >= 0) {
+       i - 1
+       console.log(i)
+       i--
+    }
+}
